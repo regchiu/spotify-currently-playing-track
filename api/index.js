58df8aa00d7805a37e48c425b7a7d00f5839ec6b
reg.chiu <regx1385@gmail.com>
2020-10-06 11:49:37 +0800

    Change Spotify logo size

--- api/index.js
+++ api/index.js
@@ -90,6 +90,8 @@ app.get('/api', async (req, res) => {
       }
 
       .card__logo {
+        min-width: 150px;
+        width: 50%;
         transform-origin: bottom;
         animation: ${cardLogoAnimation};
       }
@@ -154,7 +156,7 @@ app.get('/api', async (req, res) => {
         <div class="card__body">
           <img class="card__logo" src="data:image/svg+xml,${encodeURIComponent(
             spotifyLogoImg
-          )}" width="60%" />
+          )}" />
           <div class="card__title"><![CDATA[${cardTitle}]]></div>
           <div class="card__subtitle"><![CDATA[${cardSubtitle}]]></div>
         </div>
